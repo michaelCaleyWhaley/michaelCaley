edb6a0815f625796ffed936fce98bbc98e5f6c8f
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2019-09-16 10:17:17 +0100

    init footer

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEthernet } from "@fortawesome/free-solid-svg-icons";
 import {
   faFacebookF,
   faLinkedinIn,
   faGithubAlt,
 } from "@fortawesome/free-brands-svg-icons";
-
 
 import "./scss/footer.scss";
 import "../pages/scss/sharedLayout.scss";
@@ -13,16 +13,22 @@ import "../pages/scss/sharedLayout.scss";
 const Footer = () => (
   <footer>
     <div className="container">
-      <div className="social">
-        <div className="social__icon">
+      <div className="footer-row">
+        <FontAwesomeIcon className="footer__title" icon={faEthernet} />
+      </div>
+      <div className="footer-row footer-row--margin-bottom">
+        <div className="footer-row__icon">
           <FontAwesomeIcon icon={faFacebookF} />
         </div>
-        <div className="social__icon social__icon--padded">
+        <div className="footer-row__icon footer-row__icon--padded">
           <FontAwesomeIcon icon={faLinkedinIn} />
         </div>
-        <div className="social__icon">
+        <div className="footer-row__icon">
           <FontAwesomeIcon icon={faGithubAlt} />
         </div>
+      </div>
+      <div className="footer-row">
+        <p>Michael Caley {new Date().getFullYear()}</p>
       </div>
     </div>
   </footer>
