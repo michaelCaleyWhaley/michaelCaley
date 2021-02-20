a17c73b2cdd08bb25a28995de1b58d4376c96a16
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2021-02-20 20:08:24 +0000

    allow cross origin api

--- pages/api/zoho.js
+++ pages/api/zoho.js
@@ -1,6 +1,26 @@
 import nodemailer from 'nodemailer';
 
-export default async (req, res) => {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+export default allowCors(async (req, res) => {
   const { name, email, telephone, inquiry, emailDest } = req.body;
   if ((!name, !email, !telephone, !inquiry, !emailDest)) {
     res.status(400).json({ e: 'Incomplete details' });
@@ -25,8 +45,8 @@ export default async (req, res) => {
 
   try {
     const response = await transporter.sendMail(mailOptions);
-    res.send({ host: req.headers.host, response });
+    res.send({ response });
   } catch (e) {
     res.send({ e });
   }
-};
+});
