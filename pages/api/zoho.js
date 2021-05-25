8dc22067577cbe54f38bb87fa68d9ddcdaaee67e
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2021-05-25 12:05:38 +0100

    fallback to next config cors

--- pages/api/zoho.js
+++ pages/api/zoho.js
@@ -1,26 +1,57 @@
 import nodemailer from 'nodemailer';
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+// const allowCors = (fn) => async (req, res) => {
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   // another common pattern
+//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+//   );
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end();
+//     return;
+//   }
+//   return await fn(req, res);
+// };
 
-export default allowCors(async (req, res) => {
+// export default allowCors(async (req, res) => {
+//   const { name, email, telephone, inquiry, emailDest } = req.body;
+//   if ((!name, !email, !telephone, !inquiry, !emailDest)) {
+//     res.status(400).json({ e: 'Incomplete details' });
+//   }
+
+//   let transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     secure: true,
+//     port: 465,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_KEY,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: process.env.EMAIL_USER,
+//     to: emailDest,
+//     subject: 'webComments',
+//     html: `${name} ${email} ${telephone} ${inquiry}`,
+//   };
+
+//   try {
+//     const response = await transporter.sendMail(mailOptions);
+//     res.send({ response });
+//   } catch (e) {
+//     res.send({ e });
+//   }
+// });
+
+export default async (req, res) => {
   const { name, email, telephone, inquiry, emailDest } = req.body;
   if ((!name, !email, !telephone, !inquiry, !emailDest)) {
     res.status(400).json({ e: 'Incomplete details' });
@@ -49,4 +80,4 @@ export default allowCors(async (req, res) => {
   } catch (e) {
     res.send({ e });
   }
-});
+};
